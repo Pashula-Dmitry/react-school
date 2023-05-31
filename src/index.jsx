77579189f7f02cbd53a24a9d3a33e0bad2cad706
Dmitry <dpashula@gmail.com>
2023-05-31 12:29:40 +0300

    update packages

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './styles/styles.scss';
 import './styles/_reset.scss';
 import App from './core/App.jsx';
-import reportWebVitals from './reportWebVitals';
 import Storage from 'core/services/back-end/Storage';
 import { BrowserRouter } from 'react-router-dom';
 
+
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+
+
 (async () => {
   await Storage.createObjectStore(['albums', 'videos']);
-  ReactDOM.render(
+  root.render(
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>,
-    document.getElementById('root'),
   );
-
-  // If you want to start measuring performance in your app, pass a function
-  // to log results (for example: reportWebVitals(console.log))
-  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-  reportWebVitals();
 })();
 
 
